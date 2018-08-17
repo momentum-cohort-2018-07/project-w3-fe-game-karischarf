2447ca3e112ccccfcd0b25faa7664ac192beebe1
Kari Scharf <karischarf@gmail.com>
2018-08-17 16:56:36 -0400

    early stages of game

--- keyboarder.js
+++ keyboarder.js
@@ -1,3 +1,46 @@
+var canvas;
+var canvasContext;
+var ballX = 50;
+
+
+//wait until window loads to run this function. Grab canvas element.
+window.onload = function() {
+  canvas = this.document.getElementById('gameCanvas')
+  canvasContext = canvas.getContext('2d')
+  
+  //declaring this var in window.onload function because we won't need it outside of here
+  var framesPerSecond = 30
+  setInterval(callBoth,1000/framesPerSecond)
+
+}
+
+function callBoth() {
+  moveEverything()
+  drawEverything()
+}
+
+function moveEverything() {
+  ballX = ballX + 5
+}
+
+function drawEverything() {
+  canvasContext.fillStyle = 'pink'
+  canvasContext.fillRect(0,0,canvas.clientWidth,canvas.height)
+  canvasContext.strokeStyle = 'black'
+  canvasContext.strokeRect(150,150,300,300)
+  canvasContext.fillStyle = 'gray'
+  canvasContext.fillRect(ballX,200,50,25)
+
+}
+
+// function startGame () {
+//   let game = new Game('gameCanvas')
+//   game.start()
+// }
+
+
+
+
 class Keyboarder {
   constructor () {
     this.keyState = {}
