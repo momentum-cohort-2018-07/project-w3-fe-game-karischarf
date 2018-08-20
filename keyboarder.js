9dbda10ea448a814ce85b2bd91d9eff861f7ec68
Kari Scharf <karischarf@gmail.com>
2018-08-19 21:06:03 -0400

    made some changes

--- keyboarder.js
+++ keyboarder.js
@@ -1,6 +1,7 @@
-var canvas;
-var canvasContext;
-var ballX = 50;
+var canvas
+var canvasContext
+var ballX = 50
+var ballSpeedX = 5
 
 
 //wait until window loads to run this function. Grab canvas element.
@@ -19,27 +20,41 @@ function callBoth() {
   drawEverything()
 }
 
+//this will move the postion of the ball and increase speed
 function moveEverything() {
-  ballX = ballX + 5
+  ballX = ballX + ballSpeedX
+  if(ballX > canvas.width) {
+    ballSpeedX = -ballSpeedX
+  }  
+  if(ballX < 0) {
+    ballSpeedX = -ballSpeedX
+  }  
 }
 
 function drawEverything() {
-  canvasContext.fillStyle = 'pink'
-  canvasContext.fillRect(0,0,canvas.clientWidth,canvas.height)
-  canvasContext.strokeStyle = 'black'
-  canvasContext.strokeRect(150,150,300,300)
+  colorRect(0,0, canvas.clientWidth,canvas.height, 'pink')
+  // canvasContext.fillStyle = 'pink'
+  // canvasContext.fillRect(0,0,canvas.clientWidth,canvas.height)
+  // canvasContext.strokeStyle = 'black'
+  // canvasContext.strokeRect(100,100,400,400)
+  colorRect(100,100,400,400, 'black')
+  // canvasContext.fillStyle = 'gray'
+  // canvasContext.fillRect(ballX,200,50,25)
+  
+  //draw ball
   canvasContext.fillStyle = 'gray'
-  canvasContext.fillRect(ballX,200,50,25)
+  canvasContext.beginPath()
+  canvasContext.arc(ballX, 100, 0, 0, Math.PI*2, true)
+  // colorRect(ballX,200,50,25, 'gray')
 
 }
 
-// function startGame () {
-//   let game = new Game('gameCanvas')
-//   game.start()
-// }
-
-
-
+function colorRect(leftX,topY, width,height, drawColor) {
+  canvasContext.fillStyle = drawColor
+  canvasContext.fillRect(leftX,topY, width,height) 
+  
+}
+//starter code
 
 class Keyboarder {
   constructor () {
